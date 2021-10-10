8957898d58ba9d145c492c8cfd3d9211cb85180c
Ryan <leeyuenhigh@gmail.com>
2021-10-10 16:23:41 -0400

    Added default values

--- models/Users.js
+++ models/Users.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema(
-    {
-        username: String,
-        password: String,
-        email: String,
-        cart: { type: Array, default: [] },
-        pantry: { type: Array, default: [] },
-    },
-    { timeStamps: true }
+  {
+    username: { type: String, default: "" },
+    password: { type: String, default: "" },
+    email: { type: String, default: "" },
+    cart: { type: Array, default: [] },
+    pantry: { type: Array, default: [] },
+  },
+  { timeStamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
