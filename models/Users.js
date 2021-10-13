f1dc2ca8d39b523df0ed615702069c3f487611cc
wangyun <dianawangyun@gmail.com>
2021-10-13 12:10:14 -0500

    user model updated

--- models/Users.js
+++ models/Users.js
@@ -32,18 +32,6 @@ const UserSchema = new Schema(
     { timeStamps: true }
 );
 
-UserSchema.statics.authenticate = async function (email, password) {
-    const user = await this.findOne({ email: email });
-    if (user) {
-        const isValid = await bcrypt.compare(password, user.password);
-        if (isValid) {
-            delete user.password;
-            return user;
-        }
-    }
-    throw new UnauthorizedError("Invalid username/password");
-};
-
 UserSchema.path("email").validate(async (value) => {
     const emailCount = await mongoose.models.User.countDocuments({
         email: value,
