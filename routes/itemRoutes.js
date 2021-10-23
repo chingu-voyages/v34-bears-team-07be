04802be1ea2c60f1bae2f096fa0502dbbfb83cf2
Ryan <leeyuenhigh@gmail.com>
2021-10-22 20:44:51 -0400

    Removed post route for items

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,32 +1,6 @@
 const router = require("express").Router();
 const Item = require("../models/Items");
 
-// Create Item
-router.post("/", async (req, res) => {
-  const item = {
-    itemName: req.body.itemName,
-    // Date should be of format mm/dd/yyyy
-    expirationDate: new Date(req.body.expirationDate),
-    category: req.body.category,
-  };
-  // Check to ensure all fields are populated
-  for (const [key, value] of Object.entries(item)) {
-    if (value == null) {
-      return res.status(400).json({
-        error: { message: `Missing '${key}' in request body` },
-      });
-    }
-  }
-  // Try saving the product to the db
-  try {
-    let newItem = new Item(item);
-    newItem = await newItem.save();
-    res.status(200).json(newItem);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // Get an item by id
 router.get("/:itemId", async (req, res) => {
   // Try to find the item that the user is specifying
