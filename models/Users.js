e95ce593810571923b2fb1df14eb603a798343c0
Ryan <leeyuenhigh@gmail.com>
2021-10-29 19:51:30 -0400

    Fixed spelling for ItemSchema

--- models/Users.js
+++ models/Users.js
@@ -3,68 +3,66 @@ const { BadRequestError } = require("../expressError");
 const { Schema } = mongoose;
 
 const ItemSchema = new Schema({
-    perchaseDate: {
-        type: Date,
-        default: new Date(),
+  purchaseDate: {
+    type: Date,
+    default: new Date(),
+  },
+  expireDate: {
+    type: Date,
+    default: function () {
+      let d = new Date();
+      d.setDate(d.getDate() + 14);
+      return d;
     },
-    expireDate: {
-        type: Date,
-        default: function () {
-            let d = new Date();
-            d.setDate(d.getDate() + 14);
-            return d;
-        },
-    },
-    itemName: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        default: "",
-    },
-    qty: {
-        type: Number,
-        default: 1,
-    },
+  },
+  itemName: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    default: "",
+  },
+  qty: {
+    type: Number,
+    default: 1,
+  },
 });
 
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: [true, "Username is required."],
-        },
-        password: {
-            type: String,
-            required: [true, "Password is required."],
-        },
-        email: {
-            type: String,
-            unique: true,
-            validate: {
-                validator: function (email) {
-                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                        email
-                    );
-                },
-                message: "Please enter a valid email.",
-            },
-            required: [true, "Email is required."],
-        },
-        cart: { type: Array, default: [] },
-        // pantry: { type: Array, default: [] },
-        pantry: { type: [ItemSchema], default: [] },
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required."],
     },
-    { timeStamps: true }
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (email) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+        },
+        message: "Please enter a valid email.",
+      },
+      required: [true, "Email is required."],
+    },
+    cart: { type: Array, default: [] },
+    // pantry: { type: Array, default: [] },
+    pantry: { type: [ItemSchema], default: [] },
+  },
+  { timeStamps: true }
 );
 
 UserSchema.post("save", function (error, res, next) {
-    if (error.name === "MongoServerError" && error.code === 11000) {
-        next(new BadRequestError("This email address is already registered."));
-    } else {
-        next();
-    }
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    next(new BadRequestError("This email address is already registered."));
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema);
