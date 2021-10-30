01f61a096bad695516b4034b1ecbd8587225e4a2
wangyun <dianawangyun@gmail.com>
2021-10-30 09:37:24 -0500

    spell mistake

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ItemSchema = new Schema({
-    perchaseDate: {
+    purchaseDate: {
         type: Date,
         default: new Date(),
     },
